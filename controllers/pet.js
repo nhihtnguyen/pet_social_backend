244c7b04e1a25e83f87360603c5b34bbabb5b28c
zzf7ktx <zzf7ktx@gmail.com>
2021-12-15 21:27:17 +0700

    Add get pets by owner api

--- controllers/pet.js
+++ controllers/pet.js
@@ -1,10 +1,36 @@
 import BaseController from "./base_controller.js";
 import db from "../models/index.cjs";
-const { Pet, Post } = db;
+const { Pet, Post, User, UserPet } = db;
+import { REQUIRE_FIELDS } from "../constants/require_fields.js";
 
 export class PetController extends BaseController {
   constructor() {
     super(Pet);
+  }
+  async createOne(req, res) {
+    try {
+      // Create pet
+      const pet = await this._Model.create(req.body, {
+        fields: REQUIRE_FIELDS[this._Model.modelName],
+      });
+      // Set owner
+      if (!pet) {
+        return res.status(400).json("Pet is not created");
+      }
+      const body = req.body;
+      body.user_id = req.user.id;
+      body.pet_id = pet.id;
+      req.body = body;
+      const result = await UserPet.create(req.body, {
+        fields: REQUIRE_FIELDS[UserPet.modelName],
+      });
+      if (!result) {
+        return res.status(400).json("Failed to set owner");
+      }
+      return res.status(200).json(pet);
+    } catch (error) {
+      res.status(400).json(error);
+    }
   }
   async getPosts(req, res) {
     const where = { id: req.params.id };
@@ -18,11 +44,27 @@ export class PetController extends BaseController {
           model: Post,
           as: "Posts",
           required: true,
-          limit: 1,
         },
       ],
     });
 
     res.status(200).json(posts);
+  }
+  async getByOwner(req, res) {
+    const userId = req.query.user_id;
+    const pets = await Pet.findAll({
+      include: [
+        {
+          model: User,
+          as: "pet",
+          required: true,
+          attributes: [],
+          where: {
+            "$pet.id$": userId,
+          },
+        },
+      ],
+    });
+    res.status(200).json(pets);
   }
 }
--- routes/pets.js
+++ routes/pets.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
+import passport from "passport";
 const router = Router();
 
 import { PetController } from "../controllers/pet.js";
 
 const controller = new PetController();
 
-router.get("/", (req, res) => controller.getAll(req, res));
+//router.get("/", (req, res) => controller.getAll(req, res));
+router.get("/", (req, res) => controller.getByOwner(req, res));
 router.get("/:id", (req, res) => controller.getById(req, res));
-router.post("/", (req, res) => controller.create(req, res));
+router.post("/", passport.authenticate("jwt", { session: false }), (req, res) =>
+  controller.createOne(req, res)
+);
 router.get("/posts/:id", (req, res) => controller.getPosts(req, res));
 
 export default router;
