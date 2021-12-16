6d903499298868f1914937453b2d7946f8523118
Le Tran Dong <letrandong1199@gmail.com>
2021-12-16 22:59:28 +0700

    ADD: update post api

--- middleware/upload_image_server.mdw.js
+++ middleware/upload_image_server.mdw.js
@@ -1,13 +1,13 @@
-import upload from "../utils/multer.js";
-import multer from "multer";
+import upload from '../utils/multer.js';
+import multer from 'multer';
 
 export const uploadImageServerMiddleware = (req, res, next) => {
-  const uploader = upload.single("image");
+  const uploader = upload.single('image');
   uploader(req, res, (err) => {
     if (err instanceof multer.MulterError) {
-      return res.json({ message: "Multer error when uploading" });
+      return res.json({ message: 'Multer error when uploading' });
     } else if (err) {
-      return res.json({ message: "Unknow error when uploading" });
+      return res.json({ message: 'Unknow error when uploading' });
     }
     next();
   });
