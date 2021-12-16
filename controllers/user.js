2b8663303f71db96f915d7f707cd60dcaf6ba623
Le Tran Dong <letrandong1199@gmail.com>
2021-12-17 00:24:21 +0700

    ADD: upload avatar and background user

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,25 @@
-import BaseController from "./base_controller.js";
-import db from "../models/index.cjs";
+import BaseController from './base_controller.js';
+import db from '../models/index.cjs';
 const { User } = db;
 
 export class UserController extends BaseController {
   constructor() {
     super(User);
+  }
+  async uploadImage(req, res) {
+    try {
+      let updateField = req.path.split('/')[1];
+      let record = await this._Model.findOne({ where: { id: req.user.id } });
+      if (!record) {
+        return res.status(401).send('Unauthorized');
+      }
+      const newAvatar = await record.update({
+        updateField: req.body.media_URL,
+      });
+      res.status(200).json(newAvatar);
+    } catch (err) {
+      console.error(err);
+      res.status(400).json(err);
+    }
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,33 @@
-import { Router } from "express";
+import { Router } from 'express';
 const router = Router();
 
-import { UserController } from "../controllers/user.js";
-import passport from "passport";
+import { UserController } from '../controllers/user.js';
+import passport from 'passport';
+
+import { uploadImageCloudinaryMiddleware } from '../middleware/upload_image_cloudinary.mdw.js';
+import { uploadImageServerMiddleware } from '../middleware/upload_image_server.mdw.js';
 
 const controller = new UserController();
 
-router.get("/", passport.authenticate("jwt", { session: false }), (req, res) =>
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   controller.getAll(req, res)
 );
-router.get("/:id", (req, res) => controller.getById(req, res));
-router.post("/", (req, res) => controller.create(req, res));
+router.get('/:id', (req, res) => controller.getById(req, res));
+router.post('/', (req, res) => controller.create(req, res));
+
+router.put(
+  '/avatar',
+  passport.authenticate('jwt', { session: false }),
+  uploadImageServerMiddleware,
+  uploadImageCloudinaryMiddleware,
+  (req, res) => controller.uploadImage(req, res)
+);
+router.put(
+  '/background',
+  passport.authenticate('jwt', { session: false }),
+  uploadImageServerMiddleware,
+  uploadImageCloudinaryMiddleware,
+  (req, res) => controller.uploadImage(req, res)
+);
 
 export default router;
