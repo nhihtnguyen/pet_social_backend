ed0144a15cb5dfca8486720d1d5675de84044c92
zzf7ktx <41227962+zzf7ktx@users.noreply.github.com>
2022-02-11 14:19:17 +0700

    Update event.cjs

--- models/event.cjs
+++ models/event.cjs
@@ -8,7 +8,6 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-<<<<<<< dev_kh
       Event.hasMany(models.Paticipant, { foreignKey: "event_id" });
     }
   }
